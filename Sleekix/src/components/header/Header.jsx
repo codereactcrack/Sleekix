d9c72a3ff793f7acd3c5c7b97e9a665b78a05b7a
CodeReactCrack <codereactcrack@gmail.com>
2024-06-06 13:20:00 +0530

    Navigation design css

--- Sleekix/src/components/header/Header.jsx
+++ Sleekix/src/components/header/Header.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'; // Importing the CSS file
 
 const Header = () => {
-
   const navbar = [
-      { name:'HOME',path :'/home'},
-      { name:'ABOUT',path :'/about-us'},
-      { name:'PRODUCTS',path :'/products'},
-      { name:'SUPPORT',path :'/support'},
-      { name:'LOGIN',path :'/login'},
-  ]
+    { name: 'HOME', path: '/home' },
+    { name: 'ABOUT', path: '/about-us' },
+    { name: 'PRODUCTS', path: '/products' },
+    { name: 'SUPPORT', path: '/support' }
+  ];
+
   return (
-    <div>
-      <ul>
-        {navbar.map((item)=>{
-          return(
-           <NavLink to={item.path} key={item.name}> <li >{item.name}</li> </NavLink> 
-          )
-        })}
-      </ul>
-    </div>
-  )
+    <header className="navbar">
+      <div className="navbar-logo">
+        <NavLink to="/home">Sleekix</NavLink>
+      </div>
+      <nav>
+        <ul className="navbar-menu">
+          {navbar.map((item) => (
+            <li key={item.name} className="navbar-item">
+              <NavLink to={item.path} className="navbar-link" activeClassName="active-link">
+                {item.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className='navbar-auth'>
+        <NavLink to= 'login'>LOGIN</NavLink>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
